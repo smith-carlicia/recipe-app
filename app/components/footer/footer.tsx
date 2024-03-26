50b92e9db869356a7af357bb4ab5b1ab1ad1338d
carly smith <smith.carlicia@gmail.com>
2024-03-25 23:56:05 -0400

    added footer

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return (
+        <footer className='bg-[#2B2B2B] font-[europa] text-white font-semibold text-center tracking-widest fixed inset-x-0 bottom-0 p-2'>
+            Made with 🤍
+        </footer>
+    )
+};
+
+export default Footer;
