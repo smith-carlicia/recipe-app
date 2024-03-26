7629aa194dee6dfbd15c4c43992b654abf1eaffb
carly smith <smith.carlicia@gmail.com>
2024-03-26 14:59:30 -0400

    updated nav

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -4,32 +4,24 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 export function Nav() {
-    const pathname = usePathname()
 
     return (
-        <nav className='pt-5 pb-5 pl-3 sticky top-0'>
-            <div className="bg-transparent list-none flex flex-row justify-content-start font-[europa] space-x-4">
+        <nav className='flex flex-row justify-content-start pt-4 pb-4 pl-3 sticky top-0 shadow-xl'>
+            <div className="bg-transparent list-none flex flex-row justify-content-start space-x-4">
                 <ul>
                     <li>
-                        <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
-                            Home
-                        </Link>
+                        <Link href="/" className='font-extrabold tracking-wide text-2xl no-underline ml-1 hover:underline decoration-4 underline-offset-4	'>Home</Link>
+
                     </li>
+                </ul>
+                <ul>
                     <li>
-                        <Link
-                            className={`link ${pathname === '/about' ? 'active' : ''}`}
-                            href="/about"
-                        >
-                            About
-                        </Link>
+                        <Link href="/pages/students" className='font-extrabold tracking-wide text-2xl no-underline ml-1 hover:underline decoration-4 underline-offset-4	'>About</Link>
                     </li>
+                </ul>
+                <ul>
                     <li>
-                        <Link
-                            className={`link ${pathname === '/recipes' ? 'active' : ''}`}
-                            href="/recipes"
-                        >
-                            Recipes
-                        </Link>
+                        <Link href="/pages/staff" className='font-extrabold tracking-wide text-2xl no-underline ml-1 hover:underline decoration-4 underline-offset-4'>Recipes</Link>
                     </li>
                 </ul>
             </div>
